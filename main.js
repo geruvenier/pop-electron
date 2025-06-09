7c5fdeab5362a6c0af3c65afdfb98bca3c2b0d6c
Gerardo Venier <geruvenier@gmail.com>
2025-06-09 18:06:42 -0300

    Primer comit del proyecto

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+    icon: path.join(__dirname, 'icon.png')
+  })
+
+  win.loadURL('https://www.frvm.utn.edu.ar')
+  win.removeMenu()
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
